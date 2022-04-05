7fcb53a4469ef94fb167c476785bb9ab0f93e9d3
Oscar Guarnizo <hoskarg9619@gmail.com>
2022-04-05 08:17:31 -0500

    First commit: lpr_train files

---
+++ lpr_train/commands.sh
@@ -0,0 +1,6 @@
+# python lpd_prepare_data.py --input_dir benchmarks/endtoend/us --output_dir  openalpr/lpd  --target_width  640 --target_height 480
+
+# Download the pretrained model usa_unpruned.tlt
+#curl -LO https://api.ngc.nvidia.com/v2/models/nvidia/tao/lpdnet/versions/unpruned_v1.0/files/usa_unpruned.tlt
+
+# python -m ipykernel install --user --name=mobia-train

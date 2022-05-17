e38a98bf49a1c8218d8dd59f59e3a73da0996c2f
leduin <pepeleduin@duck.com>
2022-05-17 10:54:18 -0500

    Clean license plates croped script updated to copy accepted images to another folder

--- preparation/plates/clean_crops_by_size.py
+++ preparation/plates/clean_crops_by_size.py
@@ -1,45 +1,41 @@
 import argparse
 import os
+import shutil
 
 from PIL import Image
 
-# Loading path through parser configuration
+# Configuring path through parser configuration
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "path", help="Container directory of license plate images", type=str
+    "origin",
+    help="Container directory of license plate images",
+    type=str
+)
+parser.add_argument(
+    "destination",
+    help="Container directory of output/accepted license plate images",
+    type=str
 )
 args = parser.parse_args()
 
-# UNIQUE_LICENSE_PLATES = set()
+# Setting files, directories and folders
+FILES = sorted(os.listdir(args.origin))
+cwd = os.getcwd()
+# if output folder not exist, create
+if not os.path.exists(f"{cwd}/{args.destination}"):
+    os.makedirs(f"{cwd}/{args.destination}")
 
-FILES = sorted(os.listdir(args.path))
-
-# crop_reference_size
+# Dimensions and ratio
 channel, height, width = [3,48,96]
-# crop acceptable erros
 e_height, e_width = [5, 5]
-
-# ratio height/width
 ratio = height/width
-
-# If I increase the height by e_height then width must be
-# aceptable_height1 = height + e_height
-# aceptable_width1 = aceptable_height1/ratio
-
-# If I increase the width by e_width then height must be
-# aceptable_width2 = width + e_width
-# aceptable_height2 = ratio * aceptable_width2
-
-# TODO: check the crop dims, if the the crop is within the
-# aceptable 1 or 2, we can use the crop
-
-f = open("accepted_img.txt", "w")
 
 for lp in FILES:
     img = Image.open(f"img/{lp}")
     
-    if (img.width in range(width - e_width, width + e_width) and
-        img.height == img.width * ratio):
-        f.write(lp+"\n")
-
-f.close()
+    if ((img.width in range(width - e_width, width + e_width +1) and
+        img.height == img.width * ratio) or 
+        (img.height in range(height - e_height, height + e_height +1) and
+        img.width == img.height / ratio)):
+        shutil.copyfile(f"{cwd}/{args.origin}/{lp}",
+                        f"{cwd}/{args.destination}/{lp}")
